38713c5c19feb3b34c7e806486f1bfd39d5576c0
DELL <DELL@DESKTOP-07J22P1>
2024-05-22 09:52:50 +0530

    i added one element

--- SeleniumCRMGUIFramework/src/test/java/practice/pom/repository/Products.java
+++ SeleniumCRMGUIFramework/src/test/java/practice/pom/repository/Products.java
@@ -1,7 +1,10 @@
 package practice.pom.repository;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 public class Products {
 
@@ -9,4 +12,7 @@ public class Products {
 		// TODO Auto-generated constructor stub
 		PageFactory.initElements(driver, this);
 	}
+	@FindBy(id="name")
+	private WebElement ele1;
+	
 }
